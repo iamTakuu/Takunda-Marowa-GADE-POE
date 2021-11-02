c85568605571c7978c24cab55ff69a1d9b11c928
Taku <marowatk@gmail.com>
2021-11-02 13:13:38 +0200

    Created abstract Class Item. Adjusted the default namespace.

---
+++ Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_TASK_2
+{
+    abstract class Item : Tile
+    {
+        protected Item(int x, int y, char symbol) : base(x, y, symbol)
+        {
+        }
+    }
+}
