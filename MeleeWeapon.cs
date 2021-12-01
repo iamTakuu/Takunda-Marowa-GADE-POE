c223a184cfc225ec8a32b1f290945a419fba82bb
Takunda Tawanda Marowa (GF-Laptop) <20123325@vegaconnect.co.za>
2021-12-01 13:04:18 +0200

    Created the abstract Weapon class and it's subclass MeleeWeapon.

---
+++ MeleeWeapon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_POE
+{
+    class MeleeWeapon : Weapon
+    {
+        public MeleeWeapon(WeaponType type, int x = 0, int y = 0) : base(x, y)
+        {
+            switch (type)
+            {
+                case WeaponType.Dagger:
+                    this.WeaponTypeString = "Dagger";
+                    this.Durability = 10;
+                    this.Damage = 3;
+                    this.Cost = 3;
+                    break;
+                case WeaponType.Longsword:
+                    this.WeaponTypeString = "Longsword";
+                    this.Durability = 6;
+                    this.Damage = 4;
+                    this.Cost = 5;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public override int Range { get { return range; } set { range = 1; } }
+        //public MeleeWeapon(int x = 0, int y = 0, char symbol = 'W') : base(x, y, symbol)
+        //{
+        //}
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        public enum WeaponType
+        {
+            Dagger,
+            Longsword
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_POE
+{
+    abstract class Weapon : Item
+    {
+        private protected int damage;
+        private protected int range;
+        private protected int durability;
+        private protected int cost;
+        private protected string weaponType;
+        public int Damage { get { return damage; } set { damage = value; } }
+        public virtual int Range { get { return range; } set { range = value; } }
+        public int Durability { get { return durability; } set { durability = value; } }
+        public int Cost { get { return cost; } set { cost = value; } }
+        public string WeaponTypeString { get { return weaponType; } set { weaponType = value; } } 
+
+
+        public Weapon(int x= 0, int y= 0,char symbol = 'W') : base(x, y,symbol)
+        {
+
+        }
+
+        
+    }
+}
