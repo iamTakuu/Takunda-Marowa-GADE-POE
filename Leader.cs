224e7b847ddff2bb2c8a98bd6b548fbdc47ef0fe
Takunda Tawanda Marowa (GF-Laptop) <20123325@vegaconnect.co.za>
2021-12-01 14:45:03 +0200

    Leader Constructor

--- Leader.cs
+++ Leader.cs
@@ -8,6 +8,13 @@ namespace GADE_POE
 {
     class Leader : Enemy
     {
+        public Leader(int x, int y) : base(x, y, 'L', 20, 2)
+        {
+        }
 
+        public override MovementEnum ReturnMove(MovementEnum move = MovementEnum.NoMovement)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
